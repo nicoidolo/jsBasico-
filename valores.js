9814fb8470c39a0099c601355ca97253311fe3ad
Nico Fanjul <nicolasfanjul@icloud.com>
2022-05-02 12:03:00 -0300

    tipos de valores de js

---
+++ valores.js
@@ -0,0 +1,19 @@
+// Tipos primitivos
+40 // Numeros
+3.14
+
+"Nicolás Fanjul" // Strings (cadenas de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+'Nicolás Fanjul'
+
+true // Booleanos, estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+false
+
+null // Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante.
+undefined
+
+/* ================================================================ */
+
+// Tipos de objetos o no primitivos
+
+[1, 2, 3] // corchetes // arrays
+{ nombre: "Nicolás" } // llaves // objetos
